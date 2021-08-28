a0659e07192ce9a37c87cb15cfd7d79b97caf602
Deivis <deivisutp@gmail.com>
2021-08-28 20:23:13 -0300

    payroll

---
+++ hrpayroll/src/main/java/com/devsuperior/hrpayroll/HrpayrollApplication.java
@@ -0,0 +1,12 @@
+package com.devsuperior.hrpayroll;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrpayrollApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HrpayrollApplication.class, args);
+    }
+}
