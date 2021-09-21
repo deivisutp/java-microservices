74f101acc24f6d423c380c03f9865f4b75db568c
Deivis <deivisutp@gmail.com>
2021-09-20 23:28:34 -0300

    hr-user service

---
+++ hr-user/src/test/java/com/devsuperior/hruser/HrUserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.hruser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class HrUserApplicationTests {
+
+    @Test
+    void contextLoads() {
+
+    }
+}
